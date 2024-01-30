ba0dc2b90cdbf87fcb4e57f06d81c1bbc85c83c4
Terekhov Anatoly <anatoly.terekhov@gmail.com>
2024-01-31 01:13:33 +0300

    lk3

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -56,8 +56,8 @@ export const MediaRoom = ({
       serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
       token={token}
       connect={true}
-      video={video}
-      audio={audio}
+      video={true}
+      audio={true}
     >
       <VideoConference />
     </LiveKitRoom>
