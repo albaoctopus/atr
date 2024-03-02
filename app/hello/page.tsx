51c42a20f020f41dfd41372e65144f9dbf1087b1
albaoctopus <104466607+albaoctopus@users.noreply.github.com>
2024-03-02 19:11:14 +0300

    Update page.tsx

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const HelloComponent = () => {
- return <div> <h1 className="pl-12 flex h-screen items-center justify-center mb-4 text-5xl font-extrabold text-gray-900 dark:text-white md:text-19xl lg:text-27xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Hello World</span> </h1> </div>;
+ return <div> <h1 className="pl-12 flex h-screen items-center justify-center mb-4 text-5xl font-extrabold text-gray-900 dark:text-white md:text-19xl lg:text-27xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">One More</span> </h1> </div>;
 };
 
 
